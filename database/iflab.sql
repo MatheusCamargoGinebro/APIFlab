06f14c86491d84ff83f3405d73fedfb2549b3e45
MatheusCamargoGinebro <matheus.ginebro@aluno.ifsp.edu.br>
2024-09-26 10:48:49 -0300

    Refactor database schema and models

--- database/iflab.sql
+++ database/iflab.sql
@@ -4,118 +4,163 @@ CREATE DATABASE IF NOT EXISTS iflab;
 
 USE iflab;
 
-CREATE TABLE IF NOT EXISTS institutos (
-	-- PK
-    ID_inst INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_inst),
-    
-    -- values
-    Nome VARCHAR(128) NOT NULL,
-    Regiao CHAR(2) NOT NULL
-);
+-- =========================================== Informações do instituto ===========================================
+CREATE TABLE
+    IF NOT EXISTS campus (
+        -- PK
+        ID_campus INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_campus),
+        -- values
+        -- Informações de contato:
+        Nome VARCHAR(128) NOT NULL,
+        Telefone CHAR(11),
+        Email VARCHAR(256) NOT NULL
+    );
 
-CREATE TABLE IF NOT EXISTS users (
-	-- PK
-    ID_user INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_user),
-    
-    -- values
-    Nome VARCHAR(128) NOT NULL,
-    Email VARCHAR(256) NOT NULL,
-    Senha VARCHAR(64) NOT NULL,
-    Cargo INT CHECK (Cargo IN (1, 2, 3)),
-    imagem LONGTEXT,
-    
-    -- FK
-    ID_inst INT NOT NULL,
-    FOREIGN KEY (ID_inst) REFERENCES institutos(ID_inst)
-);
+CREATE TABLE
+    IF NOT EXISTS endereco (
+        -- PK
+        ID_endereco INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_endereco),
+        -- values
+        numero INT NOT NULL,
+        rua VARCHAR(128) NOT NULL,
+        complemento VARCHAR(128),
+        bairro VARCHAR(128) NOT NULL,
+        cidade VARCHAR(128) NOT NULL,
+        Estado CHAR(2) NOT NULL,
+        CEP CHAR(9) NOT NULL,
+        -- FK
+        ID_campus INT NOT NULL,
+        FOREIGN KEY (ID_campus) REFERENCES campus (ID_campus)
+    );
 
-CREATE TABLE IF NOT EXISTS labs (
-	-- PK
-    ID_lab INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_lab),
-    
-    -- Values
-    Sala varchar(16),
-    
-    -- FK
-    ID_inst INT NOT NULL,
-    FOREIGN KEY (ID_inst) REFERENCES institutos(ID_inst)
-);
+-- =========================================== Informações do usuário ===========================================
+CREATE TABLE
+    IF NOT EXISTS usuarios (
+        -- PK
+        ID_usuario INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_usuario),
+        -- values
+        Nome VARCHAR(128) NOT NULL,
+        Email VARCHAR(256) NOT NULL,
+        Senha VARCHAR(64) NOT NULL,
+        profilePic LONGTEXT,
+        -- FK
+        ID_campus INT NOT NULL,
+        FOREIGN KEY (ID_campus) REFERENCES campus (ID_campus)
+    );
 
-CREATE TABLE IF NOT EXISTS Elementos (
-	-- PK
-    ID_elem INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_elem),
-    
-    -- values
-    Nome VARCHAR(128) NOT NULL,
-    Quantidade DECIMAL(10,3) NOT NULL,
-    Peso_molecular DECIMAL(10,3) NOT NULL,
-    numero_cas VARCHAR(32) NOT NULL UNIQUE,
-    numero_ec VARCHAR(32) NOT NULL UNIQUE,
-    estado_fisico INT CHECK (estado_fisico IN (1, 2, 3)),
-    image LONGTEXT NOT NULL,
-    
-    -- FK
-    ID_lab INT NOT NULL,
-    FOREIGN KEY (ID_lab) REFERENCES labs(ID_lab)
-);
+CREATE TABLE
+    IF NOT EXISTS adminusuarioCampus (
+        -- PK
+        ID_relation INT NOT NULL,
+        PRIMARY KEY (ID_relation),
+        -- FK
+        ID_Responsavel INT NOT NULL,
+        FOREIGN KEY (ID_Responsavel) REFERENCES usuarios (ID_usuario),
+        ID_campus INT NOT NULL,
+        FOREIGN KEY (ID_campus) REFERENCES campus (ID_campus)
+    );
 
+-- =========================================== Informações do laboratório ===========================================
+CREATE TABLE
+    IF NOT EXISTS laboratorios (
+        -- PK
+        ID_lab INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_lab),
+        -- Values
+        Sala varchar(16),
+        -- FK
+        ID_campus INT NOT NULL,
+        FOREIGN KEY (ID_campus) REFERENCES campus (ID_campus)
+    );
 
-CREATE TABLE IF NOT EXISTS Equipamentos(
-	-- PK
-    ID_equip INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_equip),
-    
-    -- values
-    Nome VARCHAR(128) NOT NULL,
-    Descricao TEXT NOT NULL,
-    Quantidade INT NOT NULL,
-    
-	-- FK
-    ID_lab INT NOT NULL,
-    FOREIGN KEY (ID_lab) REFERENCES labs(ID_lab)
-);
+CREATE TABLE
+    IF NOT EXISTS adminusuarioLab (
+        -- PK
+        ID_relation INT NOT NULL,
+        PRIMARY KEY (ID_relation),
+        -- FK
+        ID_Responsavel INT NOT NULL,
+        FOREIGN KEY (ID_Responsavel) REFERENCES usuarios (ID_usuario),
+        ID_lab INT NOT NULL,
+        FOREIGN KEY (ID_lab) REFERENCES laboratorios (ID_lab)
+    );
 
-CREATE TABLE IF NOT EXISTS Horarios (
-	-- PK
-    ID_hor INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_hor),
-    
-    -- values
-    Tipo INT CHECK (Tipo IN (1, 2)),
-    Inicio DATE NOT NULL,
-    Fim DATE NOT NULL,
-    
-	-- FK
-    ID_lab INT NOT NULL,
-    FOREIGN KEY (ID_lab) REFERENCES labs(ID_lab),
-    ID_user INT NOT NULL,
-    FOREIGN KEY (ID_user) REFERENCES users(ID_user)
-);
+-- =========================================== Informações dos horários ===========================================
+CREATE TABLE
+    IF NOT EXISTS Horarios (
+        -- PK
+        ID_hor INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_hor),
+        -- values
+        Tipo INT CHECK (Tipo IN (1, 2)),
+        Inicio DATE NOT NULL,
+        Fim DATE NOT NULL,
+        -- FK
+        ID_lab INT NOT NULL,
+        FOREIGN KEY (ID_lab) REFERENCES laboratorios (ID_lab),
+        ID_usuario INT NOT NULL,
+        FOREIGN KEY (ID_usuario) REFERENCES usuarios (ID_usuario)
+    );
 
-CREATE TABLE IF NOT EXISTS Reserva_equipamento (
-	-- PK
-    ID_resequip INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_resequip),
-    
-    -- FK
-    ID_equip INT NOT NULL,
-    FOREIGN KEY (ID_equip) REFERENCES Equipamentos(ID_equip),
-    ID_hor INT NOT NULL,
-    FOREIGN KEY (ID_hor) REFERENCES Horarios(ID_hor)
-);
+-- =========================================== Informações dos elementos ===========================================
+CREATE TABLE
+    IF NOT EXISTS Elementos (
+        -- PK
+        ID_elem INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_elem),
+        -- values
+        Nome VARCHAR(128) NOT NULL,
+        Quantidade DECIMAL(10, 3) NOT NULL,
+        Peso_molecular DECIMAL(10, 3) NOT NULL,
+        numero_cas VARCHAR(32) NOT NULL UNIQUE,
+        numero_ec VARCHAR(32) NOT NULL UNIQUE,
+        estado_fisico INT CHECK (estado_fisico IN (1, 2, 3)),
+        imagem LONGTEXT,
+        -- Image
+        image LONGTEXT,
+        -- FK
+        ID_lab INT NOT NULL,
+        FOREIGN KEY (ID_lab) REFERENCES laboratorios (ID_lab)
+    );
 
-CREATE TABLE IF NOT EXISTS Reserva_elemento (
-	-- PK
-    ID_reslem INT NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (ID_reslem),
-    
-    -- FK
-    ID_elem INT NOT NULL,
-    FOREIGN KEY (ID_elem) REFERENCES Elementos(ID_elem),
-    ID_hor INT NOT NULL,
-    FOREIGN KEY (ID_hor) REFERENCES Horarios(ID_hor)
-);
+CREATE TABLE
+    IF NOT EXISTS Reserva_elemento (
+        -- PK
+        ID_reslem INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_reslem),
+        -- FK
+        ID_elem INT NOT NULL,
+        FOREIGN KEY (ID_elem) REFERENCES Elementos (ID_elem),
+        ID_hor INT NOT NULL,
+        FOREIGN KEY (ID_hor) REFERENCES Horarios (ID_hor)
+    );
+
+-- =========================================== Informações dos equipamentos ===========================================
+CREATE TABLE
+    IF NOT EXISTS Equipamentos (
+        -- PK
+        ID_equip INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_equip),
+        -- values
+        Nome VARCHAR(128) NOT NULL,
+        Descricao TEXT NOT NULL,
+        Quantidade INT NOT NULL,
+        -- FK
+        ID_lab INT NOT NULL,
+        FOREIGN KEY (ID_lab) REFERENCES laboratorios (ID_lab)
+    );
+
+CREATE TABLE
+    IF NOT EXISTS Reserva_equipamento (
+        -- PK
+        ID_resequip INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (ID_resequip),
+        -- FK
+        ID_equip INT NOT NULL,
+        FOREIGN KEY (ID_equip) REFERENCES Equipamentos (ID_equip),
+        ID_hor INT NOT NULL,
+        FOREIGN KEY (ID_hor) REFERENCES Horarios (ID_hor)
+    );
